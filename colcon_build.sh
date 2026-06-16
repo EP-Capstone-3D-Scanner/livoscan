7af6264f7e7ff313d2a8f735e42e5ac0b7e53ce0
hsdiep <hsdiep@ualberta.ca>
2026-06-16 14:01:41 -0600

    Added our own fork of fast-livo2. Stylized the colcon_build.sh script output.

--- colcon_build.sh
+++ colcon_build.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 set -e
 
-echo "=======Building packages======="
+_Bold_Green='\e[1;32m'
+_None='\e[0m'
+
+printf "%b|==========Colcon Build Start==========|%b\n" "$_Bold_Green" "$_None"
 
 colcon build \
     --symlink-install \
     --cmake-args -DCMAKE_BUILD_TYPE=Release
+
+printf "%b|==========Colcon Build End============|%b\n" "$_Bold_Green" "$_None"
 
 exec "$@"
